bd7cd79fed50c8f82bceba11cfa823f99ef34821
Yanis Merzouki <ymerzouki001@gmail.com>
2026-02-19 16:54:24 +0100

    feat: add installation of ausad and conversion to csv

--- src/data/examine_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel(r'datasets/open_datasets/ur3+cobotops/dataset_02052023.xlsx')
-print('Shape:', df.shape)
-print('Columns:', list(df.columns))
-print('\nFirst row:')
-print(df.iloc[0])
-print('\nData types:')
-print(df.dtypes)
